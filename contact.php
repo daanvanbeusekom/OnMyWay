fb9fc5ee26054b6d87c8012e0d6da9daa44a562d
dusthijsvdh <thijsvdh12@gmail.com>
2017-02-02 18:35:36 +0100

    Added images

--- contact.php
+++ contact.php
@@ -24,6 +24,28 @@ $result = $connection->query($SQL);
         ?>
 		</div>
 		
+		<div class="opdrachtgevers">
+			
+			<a href=""><img src="IMG/opdrachtgevers/heutink.png" id="opdrachtgever" width="100px" title="heutink" alt="heutink"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/huijbregts.jpg" id="opdrachtgever" width="100px" title="huijbregts" alt="huijbregts"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/prorail.png" id="opdrachtgever" width="100px" title="prorail" alt="prorail"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/rijkswaterstaat.png" id="opdrachtgever" width="100px" title="rijkswaterstaat" alt="rijkswaterstaat"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/techniekpromotie.jpg" id="opdrachtgever" width="100px" title="techniekpromotie" alt="techniekpromotie"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/twente.png" id="opdrachtgever" width="100px" title="twente" alt="twente"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/uterecht.png" id="opdrachtgever" width="100px" title="uterecht" alt="uterecht"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/werkenbijdefensie.png" id="opdrachtgever" width="100px" title="werken bij defensie" alt="werken_bij_defensie"></a>
+			
+			<a href=""><img src="IMG/opdrachtgevers/WoW.png" id="opdrachtgever" width="100px" title="Wegbeheerders Ontmoeten Wegbeheerders" alt="Wegbeheerders_Ontmoeten_Wegbeheerders"></a>
+			
+		</div>
+		
 <?php
 include "bottom.php";
 ?>
